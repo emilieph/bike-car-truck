7cd79e4b51f3278af3f35675eafddca942181b98
emilieph <55733007+emilieph@users.noreply.github.com>
2020-11-30 16:34:13 +0100

    Create Bicycle.php

---
+++ Bicycle.php
@@ -0,0 +1,43 @@
+<?php
+
+  // Bicycle.php
+
+  class Bicycle
+  {
+    /**
+       * @var string
+       */
+      public $color;
+
+      /**
+      * @var integer
+      */
+      public $currentSpeed;
+      /**
+      * @var integer
+      */
+      /**
+      * @var integer
+      */
+      public $nbSeats = 1;
+      /**
+      * @var integer
+      */
+      public $nbWheels = 2;
+    }
+    public function forward()
+    {
+        $this->currentSpeed = 15;
+        return "Go !";
+    }
+
+    public function brake(): string
+    {
+       $sentence = "";
+       while ($this->currentSpeed > 0) {
+           $this->currentSpeed--;
+           $sentence .= "Brake !!!";
+       }
+       $sentence .= "I'm stopped !";
+       return $sentence;
+}
